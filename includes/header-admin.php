3a74fa84fcf10324517806a752fe60a7f9956eb7
Nurhidayah <nurhidayahramadhani002@gmail.com>
2026-06-05 13:39:03 +0800

    REVISI REVISI

--- includes/header-admin.php
+++ includes/header-admin.php
@@ -1,31 +1,41 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard - Sistem Laundry</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+
 <header class="header">
-        <nav class="nav-left">
-            <ul>
-                <li>
-                    <a href="dashboard.php" class="tombol-daun"><b> Dashboard </b></a>
-                </li>
-                <li>
-                    <a href="pesanan.php" class="tombol-daun"><b> Kelola Pesanan </b></a>
-                </li>
-                <li>
-                    <a href="member.php" class="tombol-daun"><b> Member </b></a>
-                </li>
-                <li>
-                    <a href="laporan.php" class="tombol-daun"><b> Laporan </b></a>
-                </li>
-                <li>
-                    <a href="layanan.php" class="tombol-daun"><b> Layanan </b></a>
-                </li>
-                <li>
-                    <a href="edit-info.php" class="tombol-daun"><b> Info Website </b></a>
-                </li>
-            </ul>
-        </nav>
-        <div class="nav-right">
-            <ul>
-                <li>
-                    <a href="../logout.php" class="tombol-daun"><b> Logout </b></a>
-                </li>
-            </ul>
-        </div>
+    <nav class="nav-left">
+        <ul>
+            <li>
+                <a href="dashboard.php" class="tombol-daun"><b> Dashboard </b></a>
+            </li>
+            <li>
+                <a href="pesanan.php" class="tombol-daun"><b> Kelola Pesanan </b></a>
+            </li>
+            <li>
+                <a href="member.php" class="tombol-daun"><b> Member </b></a>
+            </li>
+            <li>
+                <a href="laporan.php" class="tombol-daun"><b> Laporan </b></a>
+            </li>
+            <li>
+                <a href="layanan.php" class="tombol-daun"><b> Layanan </b></a>
+            </li>
+            <li>
+                <a href="edit-info.php" class="tombol-daun"><b> Info Website </b></a>
+            </li>
+        </ul>
+    </nav>
+    <div class="nav-right">
+        <ul>
+            <li>
+                <a href="../logout.php" class="tombol-daun"><b> Logout </b></a>
+            </li>
+        </ul>
+    </div>
 </header>
