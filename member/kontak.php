92513843bf4ea00bb1afd699f43f8aace5722e38
glowyyy-07 <glorymissionary07@gmail.com>
2026-06-06 01:36:17 +0800

    revisi whatsapp di kontak member

--- member/kontak.php
+++ member/kontak.php
@@ -13,7 +13,7 @@ $info = $stmt->fetch();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Detail Pesanan - CleanCo</title>
+    <title>Kontak - CleanCo</title>
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
